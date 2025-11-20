f904fe0037ab655c1f367e88cd323e40f961c19d
YASH RAJ <yrajgecarwl@gmail.com>
2025-11-20 18:27:44 +0530

    Change height from 100vh to auto

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
                 fontSize: 16,
                 border: "1px solid #ddd",
                 borderRadius: "5px",
-                height: "100vh",
+                height: "auto",
                 width: "100%"
               }}
             />
